9a2dffdd0653318fa471849679e4e854c0216fc6
DiegoGalan <diegocanizaleza20@gmail.com>
2026-08-14 22:00:36 -0600

    feat
    agregar las dependencias

--- Desafo1DSM/app/build.gradle.kts
+++ Desafo1DSM/app/build.gradle.kts
@@ -34,12 +34,9 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
